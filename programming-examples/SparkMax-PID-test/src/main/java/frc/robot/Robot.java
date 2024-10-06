ecbb1d3ea578b621feb8d92b55a0dd1bd563249f
cgzog <chris.herzog@gmail.com>
2024-10-05 19:33:38 -0500

    SparkMax-based PID control example

---
+++ programming-examples/SparkMax-PID-test/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,157 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+/**
+ * This is a demo program showing the use of the RobotDrive class, specifically
+ * it contains the code necessary to operate a robot with tank drive.
+ */
+public class Robot extends TimedRobot {
+  private static final int deviceID = 21;
+  private CANSparkMax m_motor;
+  private SparkPIDController m_pidController;
+  private RelativeEncoder m_encoder;
+  public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM, kTolerance, kSetPoint;
+
+  @Override
+  public void robotInit() {
+
+    // initialize motor
+    m_motor = new CANSparkMax(deviceID, MotorType.kBrushless);
+
+    /**
+     * The RestoreFactoryDefaults method can be used to reset the configuration parameters
+     * in the SPARK MAX to their factory default state. If no argument is passed, these
+     * parameters will not persist between power cycles
+     */
+    m_motor.restoreFactoryDefaults();
+
+    /**
+     * In order to use PID functionality for a controller, a SparkPIDController object
+     * is constructed by calling the getPIDController() method on an existing
+     * CANSparkMax object
+     */
+    m_pidController = m_motor.getPIDController();
+
+    // Encoder object created to display position values
+    m_encoder = m_motor.getEncoder();
+
+    // PID coefficients
+    kP = 6e-5; 
+    kI = 5e-7;
+    kD = 0; 
+    kIz = 0; 
+    kFF = 0.00003; 
+    kMaxOutput = 1; 
+    kMinOutput = -1;
+    maxRPM = 5700;
+    kSetPoint = 2000;
+
+    kTolerance = 20;
+
+    // set PID coefficients
+    m_pidController.setP(kP);
+    m_pidController.setI(kI);
+    m_pidController.setD(kD);
+    m_pidController.setIZone(kIz);
+    m_pidController.setFF(kFF);
+    m_pidController.setOutputRange(kMinOutput, kMaxOutput);
+
+    // display PID coefficients on SmartDashboard
+    SmartDashboard.putNumber("P Gain", kP);
+    SmartDashboard.putNumber("I Gain", kI);
+    SmartDashboard.putNumber("D Gain", kD);
+    SmartDashboard.putNumber("I Zone", kIz);
+    SmartDashboard.putNumber("Feed Forward", kFF);
+    SmartDashboard.putNumber("Max Output", kMaxOutput);
+    SmartDashboard.putNumber("Min Output", kMinOutput);
+    SmartDashboard.putNumber("Tolerance", kTolerance);
+    SmartDashboard.putNumber("RPM", 0);
+    SmartDashboard.putNumber("SetPoint", kSetPoint);
+
+    SmartDashboard.putBoolean("Ready", false);
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    // read PID coefficients from SmartDashboard
+    double setPoint = SmartDashboard.getNumber("SetPoint", 0);
+    double p = SmartDashboard.getNumber("P Gain", 0);
+    double i = SmartDashboard.getNumber("I Gain", 0);
+    double d = SmartDashboard.getNumber("D Gain", 0);
+    double iz = SmartDashboard.getNumber("I Zone", 0);
+    double ff = SmartDashboard.getNumber("Feed Forward", 0);
+    double max = SmartDashboard.getNumber("Max Output", 0);
+    double min = SmartDashboard.getNumber("Min Output", 0);
+    double tolerance = SmartDashboard.getNumber("Tolerance", 0);
+
+    // if PID coefficients on SmartDashboard have changed, write new values to controller
+    if((setPoint != kSetPoint)) { kSetPoint = setPoint; }
+    if((p != kP)) { m_pidController.setP(p); kP = p; }
+    if((i != kI)) { m_pidController.setI(i); kI = i; }
+    if((d != kD)) { m_pidController.setD(d); kD = d; }
+    if((iz != kIz)) { m_pidController.setIZone(iz); kIz = iz; }
+    if((ff != kFF)) { m_pidController.setFF(ff); kFF = ff; }
+    if((tolerance != kTolerance)) { kTolerance = tolerance; }
+    if((max != kMaxOutput) || (min != kMinOutput)) { 
+      m_pidController.setOutputRange(min, max); 
+      kMinOutput = min; kMaxOutput = max; 
+    }
+
+    /**
+     * PIDController objects are commanded to a set point using the 
+     * SetReference() method.
+     * 
+     * The first parameter is the value of the set point, whose units vary
+     * depending on the control type set in the second parameter.
+     * 
+     * The second parameter is the control type can be set to one of four 
+     * parameters:
+     *  com.revrobotics.CANSparkMax.ControlType.kDutyCycle
+     *  com.revrobotics.CANSparkMax.ControlType.kPosition
+     *  com.revrobotics.CANSparkMax.ControlType.kVelocity
+     *  com.revrobotics.CANSparkMax.ControlType.kVoltage
+     */
+   
+    m_pidController.setReference(setPoint, CANSparkMax.ControlType.kVelocity);
+    
+    double currentVelocity = m_encoder.getVelocity();
+
+    SmartDashboard.putNumber("RPM", currentVelocity);
+    SmartDashboard.putNumber("Diff", setPoint - currentVelocity);
+    SmartDashboard.putNumber("Diff %", setPoint / (setPoint - currentVelocity) * 100);
+    SmartDashboard.putNumber("Motor Setting", m_motor.get());
+  
+    if (Math.abs(setPoint - currentVelocity) <= tolerance) {
+      SmartDashboard.putBoolean("Ready", true);  // within tolerance
+    } else {
+      SmartDashboard.putBoolean("Ready", false);  // not yet
+    }
+  }
+
+    /** This function is called periodically when disabled. */
+ @Override
+  public void disabledPeriodic() {
+
+    SmartDashboard.putNumber("RPM", 0);
+    SmartDashboard.putNumber("Diff", 0);
+    SmartDashboard.putNumber("Diff %", 0);
+    SmartDashboard.putNumber("Motor Setting", 0);
+  
+    SmartDashboard.putBoolean("Ready", false);
+  }
+  
+}
